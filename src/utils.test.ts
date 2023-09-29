f120ddf697738ed9081f8087f357a67eb99aced7
John B <johan@nexys.ch>
2023-09-29 08:22:00 +0200

    Update utils.test.ts

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -3,4 +3,6 @@ import * as T from './type';
 
 test('get subdomain', () => {
   expect(U.getSubdomain(T.Env.dev)).toEqual('dev');
+  expect(U.getSubdomain(T.Env.test)).toEqual('test');
+  expect(U.getSubdomain(T.Env.prod)).toEqual('app');
 });
