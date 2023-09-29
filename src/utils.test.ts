b496969866366a7d311bf96ec9b2ae0b54e1f705
Johan <johan@macair2020>
2023-09-29 08:25:48 +0200

    add tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,8 +1,12 @@
-import * as U from './utils';
-import * as T from './type';
+import * as U from "./utils";
+import * as T from "./type";
 
-test('get subdomain', () => {
-  expect(U.getSubdomain(T.Env.dev)).toEqual('dev');
-  expect(U.getSubdomain(T.Env.test)).toEqual('test');
-  expect(U.getSubdomain(T.Env.prod)).toEqual('app');
+test("get subdomain", () => {
+  expect(U.getSubdomain(T.Env.dev)).toEqual("dev");
+  expect(U.getSubdomain(T.Env.test)).toEqual("test");
+  expect(U.getSubdomain(T.Env.prod)).toEqual("app");
+});
+
+test("get domain", () => {
+  expect(U.getHost(T.Env.dev, "nexys", "io")).toEqual("https://dev.nexys.io");
 });
